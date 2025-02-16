d19b3c706a55373ed3fdfce718dc7c7965d13787
Francisco Barrios <azhagthott@gmail.com>
2025-02-16 13:11:23 +0700

    WIP

--- composeApp/src/androidMain/kotlin/com/fjbg/airaware/kmp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/fjbg/airaware/kmp/MainActivity.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.center
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
@@ -27,31 +35,77 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun AppPreview() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.TopCenter,
-
-        ) {
-        AqiCircle()
-        Text(
-            "123",
-            fontSize = 72.sp,
-            color = Color.Gray,
-            modifier = Modifier.align(Alignment.Center), maxLines = 1
-        )
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        UpperBox()
+        MiddleBox()
+        LowerBox()
     }
 }
 
 @Composable
+fun UpperBox() {
+    Row(
+        modifier = Modifier
+            .background(Color.Green)
+            .height(180.dp)
+            .fillMaxWidth(),
+    ) {
+        AqiText()
+    }
+}
+
+@Composable
+fun MiddleBox() {
+    Box(
+        modifier = Modifier
+            .background(Color.Red)
+            .height(320.dp),
+        contentAlignment = Alignment.Center,
+    ) {
+        AqiText()
+        AqiCircle()
+    }
+}
+
+@Composable
+fun LowerBox() {
+    Row(
+        modifier = Modifier
+            .background(Color.Blue)
+            .height(180.dp)
+            .fillMaxWidth()
+    ) {
+        AqiText()
+    }
+}
+
+@Composable
+fun AqiText() {
+    Text(
+        "888",
+        fontSize = 102.sp,
+        color = Color.Gray,
+        modifier = Modifier.padding(16.dp),
+        maxLines = 1
+    )
+}
+
+
+@Composable
 fun AqiCircle() {
-    Canvas(
-        modifier = Modifier.fillMaxSize(),
+    Canvas(modifier = Modifier
+        .fillMaxWidth()
+        .padding(16.dp),
         onDraw = {
             drawCircle(
                 color = Color.Gray,
-                radius = 360f,
-                style = Stroke(width = 24f)
+                radius = 340f,
+                style = Stroke(width = 24f),
+                center = size.center
             )
-
-        })
+        }
+    )
 }
